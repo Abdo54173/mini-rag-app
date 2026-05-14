aa289cf37f32861c5eebf1dba6c6867f3198fcd4
Abdo <Abdo54173>
2026-05-14 19:16:38 +0300

    Enhance file upload functionality: add validation, unique filename generation, and response signals; update configuration for allowed file types and sizes.

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,16 +1,44 @@
 from .BaseController import BaseController
 from fastapi import APIRouter ,FastAPI ,Depends ,UploadFile
+from src.models.enums import ResponseSignals
+from .ProjectController import ProjectController
+import re
+import os
 
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
         self.size_scale =1048576 # convert megabytes to bytes 
 
+    # Validate the uploaded file based on type and size
     def validate_uploaded_file(self ,file: UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False ,"file type not allowed"
+            return False ,ResponseSignals.FILE_TYPE_NOT_SUPPORTED.value
 
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False ,"file size exceeds limit"
+            return False ,ResponseSignals.FILE_SIZE_EXCEEDED.value
 
-        return True ,"file is valid"
+        return True ,ResponseSignals.FILE_VALIDATED_SUCCESS.value
+
+    def generate_unique_filename(self, orig_file_name: str ,project_id: str):
+        
+        random_key = self.generate_random_string()
+
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_filename(orig_file_name)
+
+        new_file_path = os.path.join(project_path, random_key + "_" + cleaned_file_name)
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(project_path, random_key + "_" + cleaned_file_name)
+
+        return new_file_path    
+
+    def get_clean_filename(self, orig_file_name: str):
+        cleaned_file_name =re.sub(r'[^\w.]','', orig_file_name.strip())
+
+        cleaned_file_name = cleaned_file_name.replace(" ","_")
+
+        return cleaned_file_name
