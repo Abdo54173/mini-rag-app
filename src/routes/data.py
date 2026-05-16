e51e625bd1b236ada912789ef8ef85d77e192ca5
Abdo <Abdo54173>
2026-05-17 01:32:09 +0300

    Implement ProcessController and processing endpoint: add file processing logic, enums for file types and response signals, and request schema for processing files.

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, UploadFile ,status
 from fastapi.responses import JSONResponse
 from src.helpers.config import get_settings, Settings
-from src.controllers import DataController ,ProjectController
+from src.controllers import DataController ,ProjectController ,ProcessController
 import aiofiles
 from src.models.enums import ResponseSignals
 import os
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -57,3 +58,31 @@ async def upload_data(
                 "file_id" : file_id
             }
         )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+        )
+    
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content ={
+                "signal" : ResponseSignals.PROCESSING_FAILED.value,
+            }
+        )
+
+    return file_chunks    
