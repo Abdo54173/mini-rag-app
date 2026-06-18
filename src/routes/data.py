0b76cd3a5752ca1c0337429ea9a59362728f2736
Abdo <abdoamrr777@gmail.com>
2026-06-18 15:34:05 +0300

    feat(models): update asset and project models to use asset_id and project_id, remove obsolete schema files

--- src/routes/data.py
+++ src/routes/data.py
@@ -25,7 +25,7 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_data(
     request: Request,
-    project_id: str,
+    project_id: int,
     file: UploadFile,
     app_settings: Settings = Depends(get_settings)
 ):
@@ -85,7 +85,7 @@ async def upload_data(
     )
 
     asset_resource = Asset(
-        asset_project_id=project.id,
+        asset_project_id=project.project_id,
         asset_type=AssetTypeEnum.FILE.value,
         asset_name=file_id,
         asset_size=os.path.getsize(file_path),
@@ -98,14 +98,14 @@ async def upload_data(
     return JSONResponse(
         content={
             "signal": ResponseSignals.FILE_UPLOADED_SUCCESS.value,
-            "file_id": str(asset_record.id),
+            "file_id": str(asset_record.asset_id),
         }
     )
 
 
 @data_router.post("/process/{project_id}")
 async def process_endpoint(
-    project_id: str,
+    project_id: int,
     process_request: ProcessRequest,
     request: Request
 ):
@@ -129,7 +129,7 @@ async def process_endpoint(
     
     if process_request.file_id:
         asset_record = await asset_model.get_asset_record(
-            asset_project_id=project.id,
+            asset_project_id=project.project_id,
             asset_name=process_request.file_id    
         )
 
@@ -143,18 +143,18 @@ async def process_endpoint(
             
 
         project_files_ids= {
-            asset_record.id: asset_record.asset_name
+            asset_record.asset_id: asset_record.asset_name
         }
         
     else:
         
         project_files= await asset_model.get_all_project_assets(
-            asset_project_id=project.id,
+            asset_project_id=project.project_id,
             asset_type= AssetTypeEnum.FILE.value,
         )
 
         project_files_ids ={
-            record.id : record.asset_name
+            record.asset_project_id : record.asset_name
             for record in project_files
         }
     
@@ -179,7 +179,7 @@ async def process_endpoint(
 
     if do_reset == 1:
             _ = await chunk_model.delete_chunks_by_project_id(
-                project_id=project.id
+                project_id=project.project_id
             )
 
     for asset_id, file_id in project_files_ids.items() :
@@ -212,7 +212,7 @@ async def process_endpoint(
                 chunk_text=chunk.page_content,
                 chunk_metadata=chunk.metadata,
                 chunk_order=i + 1,
-                chunk_project_id=project.id,
+                chunk_project_id=project.project_id,
                 chunk_asset_id=asset_id
             )
             for i, chunk in enumerate(file_chunks)
