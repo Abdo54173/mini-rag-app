4021fc01093f5d50bddd247b29960bdd0fdd530c
Abdo <abdoamrr777@gmail.com>
2026-07-03 16:34:54 +0300

    feat(config): add VECTOR_DB_BACKEND_LITERAL to settings and update VectorDBEnums

--- src/stores/vectordb/VectorDBEnums.py
+++ src/stores/vectordb/VectorDBEnums.py
@@ -2,7 +2,20 @@ from enum import Enum
 
 class VectorDBEnum(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "dot"
+
+class PgVectorTableScemeEnums(Enum):
+    ID = 'id'
+    TEXT = 'text'
+    VECTOR = 'vector'
+    CHUNK_ID = 'chunk_id'
+    METADATA = 'metadata'
+    _PREFIX = 'pgvector'
+
+class PgVectorIndexTypeEnums(Enum):
+    HNSW = "hnsw"
+    IVFFLAT = "ivfflat"
